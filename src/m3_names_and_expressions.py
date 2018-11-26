35fc7cc94f7913ddff2e37c190cf3f1ef81c77f5
chenx16 <chenx16@rose-hulman.edu>
2018-11-26 14:50:53 -0500

    Done m3

--- src/m3_names_and_expressions.py
+++ src/m3_names_and_expressions.py
@@ -53,7 +53,7 @@ print(2**0.5)
 
 
 ###############################################################################
-# TODO: 5.
+# done: 5.
 #   Every object has a TYPE and a VALUE.  For example,
 #   for the object that is computed by  math.sqrt(2):
 #      Its TYPE is float  (which is shorthand for "floating point number").
@@ -92,7 +92,19 @@ print(2**0.5)
 # After you have written and run the code to learn the TYPE
 # of each of the above, change the above _TODO_ to DONE.
 ###############################################################################
+print(type("hello"))
+print(type('hello'))
+print(type('a b c'))
 print(type(3+3))
+print(type("3" + "3"))
+print(type(2 ** 100))
+print(type(2.0 ** 100))
+print(type(math.sin(8)))
+print(type(math.sin))
+print(type(print))
+print(type(math))
+print(type('math'))
+
 ###############################################################################
 #
 # done: 6.
